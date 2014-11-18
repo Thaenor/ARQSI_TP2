e5ad652f10f0c70aa57982abd08accff5044806e
Francisco Santos <1111315@isep.ipp.pt>
2014-11-18 03:37:30 +0000

    issue #4, #5, #6 Created Model (with DB inserts), Controllers and views for Album, Users and Shopping Carts

---
+++ IDEIMusic/DAL/MusicContext.cs
@@ -0,0 +1,18 @@
+﻿using IDEIMusic.Models;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace IDEIMusic.DAL
+{
+    public class MusicContext : DbContext
+    {
+        public DbSet<Album> Albums { get; set; }
+        public DbSet<ShoppingCart> ShoppingCarts { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
---
+++ IDEIMusic/Models/User.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IDEIMusic.Models
+{
+    /// <summary>
+    /// Model for User:
+    /// UserID: auto-incremented ID
+    /// Name: name of user
+    /// Type: an ID that defines the type of user
+    /// 1- user (view & access to shop cart)
+    /// 2- manager (CRUD access to all data)
+    /// 3- administrator (view all users)
+    /// 
+    /// Password: (TODO: encrypt this field) user password
+    /// ApiKey: (optional) if type=1 then an API must be generated
+    /// PurchaseHistory: A list containing all the albuns the user has purchased
+    /// </summary>
+    public class User
+    {
+        public int UserID { get; set; }
+        public string Name { get; set; }
+        //type: 1-user 2-manager 3-administrator
+        public int Type { get; set; }
+        public string password { get; set; }
+        public string ApiKey { get; set; }
+
+        public virtual ICollection<Album> PurchaseHistory { get; set; }
+    }
+}
