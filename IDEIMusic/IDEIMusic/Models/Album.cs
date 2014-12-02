58a76e884da739cd3c51ed6f324d638f717e6e11
Francisco Santos <1111315@isep.ipp.pt>
2014-12-02 22:50:19 +0000

    Added Album class

---
+++ IDEIMusic/IDEIMusic/Models/Album.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IDEIMusic.Models
+{
+    public class Album
+    {
+        public int AlbumID { get; set; }
+        public string Name { get; set; }
+        public string Artist { get; set; }
+        public int StockAmmount { get; set; }
+        public float UnitPrice { get; set; }
+    }
+}
