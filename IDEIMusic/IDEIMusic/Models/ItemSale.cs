68c2f751f9b26df793495ca2688f1a06a42395ff
Francisco Santos <1111315@isep.ipp.pt>
2014-12-02 22:50:54 +0000

    Added ItemSale intermidiate model class

---
+++ IDEIMusic/IDEIMusic/Models/ItemSale.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IDEIMusic.Models
+{
+    public class ItemSale
+    {
+        public ICollection<Sale> SaleID { get; set; }
+        public ICollection<Album> AlbumID { get; set; }
+        public int Quantity { get; set; }
+    }
+}
