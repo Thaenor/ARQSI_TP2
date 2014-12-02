f8814461b23f92da0f98cb1a5d1cee6b79fbc16c
Francisco Santos <1111315@isep.ipp.pt>
2014-12-02 22:50:33 +0000

    Added Sale class

---
+++ IDEIMusic/IDEIMusic/Models/Sale.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IDEIMusic.Models
+{
+    public class Sale
+    {
+        public int SaleID { get; set; }
+        public virtual ICollection<ApplicationUser> UserID { get; set; }
+        public virtual ICollection<ItemSale> ItemSaleID { get; set; }
+        public DateTime PurchaseDate { get; set; }
+    }
+}
