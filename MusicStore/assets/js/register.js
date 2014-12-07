e6cafab056f54c15608661c8107fcd2f85b467ea
Thaenor <1111315@isep.ipp.pt>
2014-12-07 16:52:12 +0000

    Registation validation and ajax
    Register.js validates and makes ajax call to server

--- MusicStore/assets/js/register.js
+++ MusicStore/assets/js/register.js
@@ -1,1 +1,70 @@
-/*Register class*/
+var xmlHttpObj;
+var doctext;
+
+function openRegister(){
+
+    var cont = document.getElementById('RegisterWidget');
+    cont.style.visibility = "visible";
+}
+
+function registationValidator(){
+
+    var username = document.getElementById('regUsername').value;
+    var password = document.getElementById('regPassword').value;
+    var rePassword = document.getElementById('regPassword2').value;
+
+    if(username != '' && password != '' && rePassword != '' && password == rePassword){
+        MakeRegisterXMLHTTPCall(username,password, rePassword);
+    }else {
+        var ele = document.getElementById('errorlist');
+        ele.innerHTML = 'we couldn\'t find the server, please review your user name and password while we try to figure where he went.';
+        ele.style.visibility = 'visible';
+    }
+}
+
+
+/******************************************************************************/
+
+ function CreateRegisterXmlHttpRequestObject( )
+ {
+   // detecção do browser simplificada
+   // e sem tratamento de excepções
+   xmlHttpObj=null;
+   if (window.XMLHttpRequest) // IE 7 e Firefox
+     {
+       xmlHttpObj=new XMLHttpRequest()
+
+     }
+     else if (window.ActiveXObject) // IE 5 e 6
+       {
+         xmlHttpObj=new ActiveXObject("Microsoft.XMLHTTP")
+       }
+       return xmlHttpObj;
+ };
+
+function MakeRegisterXMLHTTPCall(username,password, Repassword)
+{
+    xmlHttpObj = CreateRegisterXmlHttpRequestObject();
+
+    if (xmlHttpObj)
+    {
+        // Definição do URL para efectuar pedido HTTP - método GET
+        xmlHttpObj.open("GET","Register.php"+'?username='+username+'&password='+password+'&REpassword='+Repassword,true);
+
+        // Registo do EventHandler
+        xmlHttpObj.onreadystatechange = RegisterstateHandler();
+        xmlHttpObj.send(null);
+    }
+}
+
+function RegisterstateHandler()
+{
+    if ( xmlHttpObj.readyState == 4 && xmlHttpObj.status == 200) // resposta do servidor completa
+    {
+        // propriedade responseText que devolve a resposta do servidor
+        doctext = xmlHttpObj.responseText;
+
+        console.log(doctext);
+        if(doctext=='success!'){console.log('success!!!');}
+    }
+}
