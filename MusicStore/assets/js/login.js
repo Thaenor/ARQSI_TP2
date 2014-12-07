1d90cb762d18dcaa05b337ee904cd60b03c85f29
Thaenor <1111315@isep.ipp.pt>
2014-12-07 12:28:44 +0000

    Code cleanup

--- MusicStore/assets/js/login.js
+++ MusicStore/assets/js/login.js
@@ -7,6 +7,9 @@ var Tprice;
 var username;
 var cash;
 
+/******************************************************************************/
+
+
 function preValidateLogin(){
 
   var username = document.getElementById('username').value;
@@ -20,6 +23,9 @@ function preValidateLogin(){
       ele.style.visibility = 'visible';
   }
 }
+
+/******************************************************************************/
+
 
 function CreateXmlHttpRequestObject( )
 {
@@ -38,6 +44,9 @@ function CreateXmlHttpRequestObject( )
       return xmlHttpObj;
 };
 
+/******************************************************************************/
+
+
 function MakeXMLHTTPCall(username,password)
 {
       xmlHttpObj = CreateXmlHttpRequestObject();
@@ -52,6 +61,9 @@ function MakeXMLHTTPCall(username,password)
           xmlHttpObj.send(null);
         }
 }
+
+/******************************************************************************/
+
 
 function stateHandler()
 {
@@ -79,70 +91,4 @@ function stateHandler()
     }
 }
 
-function printCatalog(json){
-
-    var container = document.getElementById('CatalogDisplay');
-    container.innerHTML = "";
-    for(var x=0; x<json.Catalog.length; x++){
-        container.innerHTML += json.Catalog[x].Name+" | "+json.Catalog[x].price;
-        container.innerHTML += " &nbsp; ";
-        var n = json.Catalog[x].Name;
-        var p = json.Catalog[x].price;
-        //var stringer =  '<input type="button" value="buy" onclick="addToCart("'+n+'",'+p+')">';
-        //var stringer = '<div id="buyButtCont">buy this shit</div>';
-        //container.innerHTML += stringer;
-        //container.innerHTML += "<br/>";
-
-        var link = document.createElement('a');
-        var method = "addToCart("+'"'+n+'"'+","+'"'+p+'"'+")";
-        var linkText = document.createTextNode('add to cart');
-        
-        link.appendChild(linkText);
-        container.appendChild(link);
-    }
-}
-
-function addToCart(product, price){
-alert(product+price);
-    cart.push(product);
-    Tprice+=price;
-    var ele = document.getElementById('cashCont');
-    ele.innerHTML = cash-price;
-    if(cash < 0) {alert("you're broke!");}
-
-}
-
-function viewCart(){
-    var ele = document.getElementById('CartSpace');
-
-    for(x in cart){
-        ele.innerHTML+= cart[x] + '<br/>'
-    }
-    ele.innerHTML+='<input type="button" value="buy(view result on console)" onclick="buy()">';
-    ele.style.visibility = 'visible';
-}
-
-function buy(){
-    var sendJSON = JSON.stringify(cart);
-    console.log = sendJSON;
-
-    /*
-    * 1. send this data to the server as new ajax request
-    * 2. decrease quantity in albums and make sure everything was ok
-    * 3. (optionally) return the user cash amount and compare to make sure the math was right
-    * */
-}
-
-
-/*
-* programming notes:
-*http://jsoneditoronline.org/#
-* { "User" : { "Name"  : "John Doe", "cash" : 700 },
- "Catalog"  : [
- { "Name"  : "Album1", "price" : 100, "Discount" : 10 },
- { "Name"  : "Album2", "price" : 130, "Discount" : 20 },
- { "Name"  : "Album3", "price" : 130, "Discount" : 20 },
- { "Name"  : "Album4", "price" : 130, "Discount" : 20 }
- ]
- }
-* */
+/******************************************************************************/
